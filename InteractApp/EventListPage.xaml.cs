29de1775fb0caf266fa21b493d612cf48bb08aa7
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-09-19 20:31:05 -0700

    EventListPage: ViewCell layout improvements/testing, still have some ideas

--- InteractApp/EventListPage.xaml.cs
+++ InteractApp/EventListPage.xaml.cs
@@ -8,8 +8,9 @@ namespace InteractApp
 {
 	public partial class EventListPage : ContentPage
 	{
+		private static readonly string EventDesc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 		public List<Event> SampleList =  new List<Event> () {
-			Event.newEvent(0, "http://bloggingtips.moneyreigninc.netdna-cdn.com/wp-content/uploads/2014/12/Event-Blogging-Strategies.jpg", "Event0", new DateTime(), "Fremont, CA", "Test Event 0", new List<String> (){ "service" })
+			Event.newEvent(0, "http://bloggingtips.moneyreigninc.netdna-cdn.com/wp-content/uploads/2014/12/Event-Blogging-Strategies.jpg", "Event0", DateTime.Now, "Fremont, CA", EventDesc, new List<String> (){ "service" })
 		};
 
 		public EventListPage ()
@@ -23,7 +24,7 @@ namespace InteractApp
 				((ListView)sender).SelectedItem = null; // de-select the row
 			};
 
-			Padding = new Thickness (0,Device.OnPlatform(20, 0, 0, 0),0,0);
+			Padding = new Thickness (0,Device.OnPlatform(20, 0, 0),0,0);
 		}
 	}
 }
