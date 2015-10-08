16514f432cbd35d3a3fe70b3b208e2a8fd530695
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-10-08 00:47:17 -0700

    Cleaned up EventInfoPage

--- InteractApp/EventInfoPage.xaml.cs
+++ InteractApp/EventInfoPage.xaml.cs
@@ -10,47 +10,14 @@ namespace InteractApp
 		public EventInfoPage (Event e)
 		{
 			InitializeComponent ();
+			EventInfo.BindingContext = e;
 			this.Title = "Event Info";
-			LoadEvent (e);
-		}
+			evtTagLabel.Text = "Tags:  " + String.Join("    ", e.Tags);
 
-		public void LoadEvent(Event evt) 
-		{
-
-			evtName.Text = "Default Name";
-			if (evt.Name != null) {
-				evtName.Text = evt.Name;
-			}
-
-			if (evt.ImageUri != null) {
-				evtPic.Source = evt.ImageUri;
-			} else {
-				evtPic.Source= (FileImageSource) ImageSource.FromFile("irvington1.jpg");
-			}
-
-			evtDate.Text = "Default Date";
-			if (evt.Date != null) {
-				evtDate.Text = "Date: " + evt.Date;
-			}
-
-			evtLoc.Text = "Location: Irvington High School";
-			if (evt.Location != null) {
-				evtLoc.Text  = "Location: " + evt.Location;
-			}
-
-			evtDescLabel.Text = "Event Description";
-			if (evt.Desc != null) {
-				evtDesc.Text = evt.Desc;
-			}
-
-			if (evt.Tags != null) {
-				string tagString = "Tags:  ";
-				for (int ii=0; ii < evt.Tags.Count; ii++) {
-					tagString = tagString + evt.Tags [ii] + "    ";
-				}
-				evtTagLabel.Text = tagString;
-			}
-
+			ToolbarItems.Add(new ToolbarItem {
+				Text = "RSVP",
+				Order = ToolbarItemOrder.Primary,
+			});
 		}
 	}
 }
