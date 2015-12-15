31213c01751dc9c49c59921725b7156ce5e1fe12
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2015-12-15 00:11:03 -0800

    iOS: database code hooray

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -10,14 +10,20 @@ namespace InteractApp.iOS
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
+		EventManager eventManager;
+
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init ();
 
 			// Code for starting up the Xamarin Test Cloud Agent
 			#if ENABLE_TEST_CLOUD
-			Xamarin.Calabash.Start();
+			Xamarin.Calabash.Start ();
 			#endif
+
+			eventManager = new EventManager (ParseStorage.Default);
+
+			App.SetEventManager (eventManager);
 
 			LoadApplication (new App ());
 
---
+++ iOS/ParseStorage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Parse;
+
+namespace InteractApp.iOS
+{
+	public class ParseStorage : IParseStorage
+	{
+		static ParseStorage eventServiceInstance = new ParseStorage ();
+
+		public static ParseStorage Default { get { return eventServiceInstance; } }
+
+		public List<Event> Items { get; private set; }
+
+		// Constructor
+		protected ParseStorage ()
+		{
+			Items = new List<Event> ();
+
+			ParseObject.RegisterSubclass<Event> ();
+			ParseClient.Initialize (Constants.ApplicationId, Constants.Key);
+		}
+
+		public static async Task<List<Event>> GetAllEvents ()
+		{
+			var query = new ParseQuery<Event> ().OrderBy ("Date");
+			var ie = ((IEnumerable<Event>)await query.FindAsync ()).ToList ();
+
+			return ie;
+		}
+
+		async public Task<List<Event>> RefreshDataAsync ()
+		{
+			var query = new ParseQuery<Event> ().OrderBy ("Date");
+			Items = ((IEnumerable<Event>)await query.FindAsync ()).ToList ();
+
+			return Items;
+		}
+
+		public async Task SaveEventAsync (Event eventObj)
+		{
+			await eventObj.SaveAsync ();
+		}
+
+		public async Task<Event> GetEventAsync (string id)
+		{
+			var query = new ParseQuery<Event> ().WhereEqualTo ("objectId", id);
+			var t = (Event)await query.FirstAsync ();
+			return t;
+		}
+
+		public async Task DeleteEventAsync (Event eventObj)
+		{
+			try {
+				await eventObj.DeleteAsync ();
+			} catch (Exception e) {
+				Console.Error.WriteLine (@"				ERROR {0}", e.Message);
+			}
+		}
+	}
+}
+
