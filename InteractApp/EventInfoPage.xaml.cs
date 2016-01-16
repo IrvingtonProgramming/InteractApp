733aed7afbdaff5c79b226f73c8b9ecb71fdeb97
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2016-01-16 11:16:06 -0800

    Fix #24: Only subscribe to Invalid URI messages once

--- InteractApp/EventInfoPage.xaml.cs
+++ InteractApp/EventInfoPage.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using Xamarin.Forms;
+using Android.Support.Design.Widget;
+using Java.Security.Cert;
 
 namespace InteractApp
 {
 	public partial class EventInfoPage : ContentPage
 	{
+		private static bool _subscribed = false;
+
 		readonly EventInfoPageViewModel _viewModel;
 
 		public EventInfoPageViewModel ViewModel {
@@ -25,9 +29,12 @@ namespace InteractApp
 				Command = new Command (ViewModel.RSVP),
 			});
 
-			MessagingCenter.Subscribe<EventInfoPageViewModel> (this, "Invalid URI", (sender) => {
-				DisplayAlert ("Oops", "Invalid URI. Either you can't RSVP to this, or we screwed up. If you believe we screwed up, please contact Interact.", "YESSIR");
-			});
+			if (!_subscribed) {
+				MessagingCenter.Subscribe<EventInfoPageViewModel> (this, "Invalid URI", (sender) => {
+					DisplayAlert ("Oops", "Invalid URI. Either you can't RSVP to this, or we screwed up. If you believe we screwed up, please contact Interact.", "YESSIR");
+				});
+				_subscribed = true;
+			}
 		}
 	}
 }
