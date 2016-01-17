37ed034fe9ee18dee79f7bc1a334920c145ca48f
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2016-01-17 14:41:07 -0800

    FilterPage: use MVVM, persist filters

--- InteractApp/FilterPage.xaml.cs
+++ InteractApp/FilterPage.xaml.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Android.Util;
 
 namespace InteractApp
 {
-	public partial class FilterPage : ContentPage
+	public class FilterPageBase : ViewPage<FilterPageViewModel>
 	{
-		public static readonly int[] AREA_CHOICES = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+	}
+
+	public partial class FilterPage : FilterPageBase
+	{
+		static readonly int[] AREA_CHOICES = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+		readonly Entry[] Entries;
+		readonly DatePicker[] DatePickers;
+		readonly Picker[] Pickers;
+
 
 		public FilterPage ()
 		{
@@ -17,8 +27,25 @@ namespace InteractApp
 			foreach (int i in AREA_CHOICES) {
 				FilterAreaPicker.Items.Add (i.ToString ());
 			}
+			// Have to set index AFTER picker has been populated
+			FilterAreaPicker.SelectedIndex = (int)Application.Current.Properties [Key.AREA];
+
+			Entries = new [] {
+				FilterNameEntry,
+				FilterSchoolEntry,
+			};
+
+			DatePickers = new [] {
+				FilterFromDatePicker,
+				FilterToDatePicker,
+			};
+
+			Pickers = new [] {
+				FilterAreaPicker,
+			};
 
 			FilterClearAllButton.Clicked += ClearAllClicked;
+			FilterApplyButton.Clicked += ApplyClicked;
 		}
 
 		void ClearAllClicked (object sender, EventArgs e)
@@ -34,6 +61,11 @@ namespace InteractApp
 			foreach (Switch s in switches) {
 				s.IsToggled = false;
 			}
+		}
+
+		void ApplyClicked (Object sender, EventArgs e)
+		{
+			return;
 		}
 	}
 }
---
+++ InteractApp/ViewModels/FilterPageViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace InteractApp
+{
+	public class FilterPageViewModel : INotifyPropertyChanged, IViewModel
+	{
+		public FilterPageViewModel ()
+		{
+		}
+
+		public string Name {
+			get {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.NAME, out v);
+				return (string)v;
+			}
+
+			set {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.NAME, out v);
+				if ((string)v != value) {
+					Application.Current.Properties [Key.NAME] = value;
+					RaisePropertyChanged ("Name");
+				}
+			}
+		}
+
+		public DateTime FromDate {
+			get {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.FROM_DATE, out v);
+				return (DateTime)((v) ?? DateTime.Now);
+			}
+
+			set {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.FROM_DATE, out v);
+				if (((DateTime)((v) ?? DateTime.Now)) != value) {
+					Application.Current.Properties [Key.FROM_DATE] = value;
+					RaisePropertyChanged ("FromDate");
+				}
+			}
+		}
+
+		public DateTime ToDate {
+			get {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.TO_DATE, out v);
+				return (DateTime)((v) ?? DateTime.Now);
+			}
+
+			set {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.TO_DATE, out v);
+				if (((DateTime)((v) ?? DateTime.Now)) != value) {
+					Application.Current.Properties [Key.TO_DATE] = value;
+					RaisePropertyChanged ("ToDate");
+				}
+			}
+		}
+
+		public string School {
+			get {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.SCHOOL, out v);
+				return (string)v;
+			}
+
+			set {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.SCHOOL, out v);
+				if ((string)v != value) {
+					Application.Current.Properties [Key.SCHOOL] = value;
+					RaisePropertyChanged ("School");
+				}
+			}
+		}
+
+		public int Area {
+			get {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.AREA, out v);
+				return (int)(v ?? -1);
+			}
+
+			set {
+				object v;
+				Application.Current.Properties.TryGetValue (Key.AREA, out v);
+				if (((int)(v ?? -1)) != value) {
+					Application.Current.Properties [Key.AREA] = value;
+					RaisePropertyChanged ("Area");
+				}
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void RaisePropertyChanged (string propName)
+		{
+			if (PropertyChanged != null) {
+				PropertyChanged (this, new PropertyChangedEventArgs (propName));
+			}
+		}
+	}
+
+	public sealed class Key
+	{
+		public static readonly string NAME = "Name";
+		public static readonly string FROM_DATE = "FromDate";
+		public static readonly string TO_DATE = "ToDate";
+		public static readonly string SCHOOL = "School";
+		public static readonly string AREA = "Area";
+	}
+}
+
