a7077997c51c2e8a636af23b1c4bb4331a6f4f0d
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2016-01-18 10:49:22 -0800

    Fix #25: Remove Location, Add school/areas and update views accordingly

--- InteractApp/Models/Event.cs
+++ InteractApp/Models/Event.cs
@@ -10,8 +10,9 @@ namespace InteractApp
 	public class Event : ParseObject
 	{
 		public static readonly string DEFAULT_NAME = "An Error Has Occurred";
-		public static readonly string DEFAULT_LOCATION = "Hooli Headquarters";
 		public static readonly string DEFAULT_DESC = "If you are seeing this and you're a user, we probably screwed up. Please try again or contact Interact Club.";
+		public static readonly int[] DEFAULT_AREAS = { -1 };
+		public static readonly string DEFAULT_SCHOOL = "Hooli Headquarters";
 		public static readonly List<string> DEFAULT_TAGS = new List<string> () { "Error", "Event" };
 		public static readonly string DEFAULT_FORM_URI = "";
 
@@ -39,14 +40,20 @@ namespace InteractApp
 			private set { SetProperty<DateTime> (value); }
 		}
 
-		[ParseFieldName ("Location")]
-		public string Location {
+		[ParseFieldName ("Desc")]
+		public string Desc {
 			get { return GetProperty<string> (); }
 			private set { SetProperty<string> (value); }
 		}
 
-		[ParseFieldName ("Desc")]
-		public string Desc {
+		[ParseFieldName ("Areas")]
+		public IList<int> Areas {
+			get { return GetProperty<IList<int>> ().ToList (); }
+			private set { SetProperty<IList<int>> ((IList<int>)value); }
+		}
+
+		[ParseFieldName ("School")]
+		public string School {
 			get { return GetProperty<string> (); }
 			private set { SetProperty<string> (value); }
 		}
@@ -63,26 +70,29 @@ namespace InteractApp
 			private set { SetProperty<string> (value); }
 		}
 
-		public string LocationDate { get { return string.Format ("{0} - {1}", this.Location, this.Date.ToShortDateString ()); } }
+		public string DateAreasSchool { get { return string.Format ("{0} | Areas {1} | {2}", this.Date.ToShortDateString (), String.Join (",", this.Areas), this.School); } }
+
+		public string AreasSchool { get { return string.Format ("Areas {0} | {1}", String.Join (",", this.Areas), this.School); } }
 
 		public Event ()
 		{
 		}
 
-		public Event (string EImageUri, string EName, DateTime EDate, string ELocation, string EDesc, List<string> ETags, string EFormUri)
+		public Event (string EImageUri, string EName, DateTime EDate, string EDesc, int[] EAreas, string ESchool, List<string> ETags, string EFormUri)
 		{
 			this.ImageUri = EImageUri;
 			this.Name = EName;
 			this.Date = EDate;
-			this.Location = ELocation;
 			this.Desc = EDesc;
+			this.Areas = EAreas;
+			this.School = ESchool;
 			this.Tags = ETags;
 			this.FormUri = EFormUri;
 		}
 
-		public static Event newEvent (string EImageUri, string EName, DateTime EDate, string ELocation, string EDesc, List<string> ETags, string EFormUri)
+		public static Event newEvent (string EImageUri, string EName, DateTime EDate, string EDesc, int[] EAreas, string ESchool, List<string> ETags, string EFormUri)
 		{
-			Event e = new Event (EImageUri, EName, EDate, ELocation, EDesc, ETags, EFormUri);
+			Event e = new Event (EImageUri, EName, EDate, EDesc, EAreas, ESchool, ETags, EFormUri);
 			// TODO: Add restrictions?
 			return e;
 		}
@@ -90,9 +100,9 @@ namespace InteractApp
 		public static Event newErrorEvent (string ExceptionString)
 		{
 			if (ExceptionString != null) {
-				return new Event (null, DEFAULT_NAME, DateTime.Now, DEFAULT_LOCATION, ExceptionString, DEFAULT_TAGS, DEFAULT_FORM_URI);
+				return new Event (null, DEFAULT_NAME, DateTime.Now, ExceptionString, DEFAULT_AREAS, DEFAULT_SCHOOL, DEFAULT_TAGS, DEFAULT_FORM_URI);
 			}
-			return new Event (null, DEFAULT_NAME, DateTime.Now, DEFAULT_LOCATION, DEFAULT_DESC, DEFAULT_TAGS, DEFAULT_FORM_URI);
+			return new Event (null, DEFAULT_NAME, DateTime.Now, DEFAULT_DESC, DEFAULT_AREAS, DEFAULT_SCHOOL, DEFAULT_TAGS, DEFAULT_FORM_URI);
 		}
 	}
 }
