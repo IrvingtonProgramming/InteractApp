ab10aa1a2276ce16d28eccec945230c308ef628f
Vaibhav Aggarwal <vaibhavagg98@gmail.com>
2016-01-18 17:58:08 -0800

    Code cleanup/import optimizations

--- InteractApp/EventListPage.xaml.cs
+++ InteractApp/EventListPage.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Threading.Tasks;
 
 using Xamarin.Forms;
 
 using InteractApp;
-using System.Threading.Tasks;
 
 namespace InteractApp
 {
--- InteractApp/ViewModels/EventInfoPageViewModel.cs
+++ InteractApp/ViewModels/EventInfoPageViewModel.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+
 using Xamarin.Forms;
-using System;
 
 namespace InteractApp
 {
